a56d61366ca8edbdc8590c30457c1973c3fad4e3
088haizi <088haizi@gmail.com>
2015-04-08 19:44:06 +0800

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root              'static_pages#home'
   get 'help'    =>  'static_pages#help'
   get 'about'   =>  'static_pages#about'
@@ -7,8 +9,12 @@ Rails.application.routes.draw do
   
   get 'signup'  =>  'users#new'
   
+  get 'login'   =>  'sessions#new'
+  post 'login'  =>  'sessions#create'
+  delete 'logout'  =>  'sessions#destroy'
+  
   resources :users
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation: firsts created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,29 @@ require 'minitest/reporters'
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
+  include ApplicationHelper
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  include ApplicationHelper
-  # Add more helper methods to be used by all tests here...
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+  
+  def log_in_as(user, option = {})
+    password    = option[:password]     || 'password'
+    remember_me = option[:remember_me]  || '1'
+    if integration_test?
+      post login_path, session: { email:      user.email,
+                                  password:   password,
+                                  remember_me:remember_me }
+    else
+      session[:user_id] = user_id
+    end
+  end
+  
+  private
+  
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
