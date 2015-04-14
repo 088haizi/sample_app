ea17031f82206ad3dfcd9afd6806ca00e2297a0b
088haizi <088haizi@gmail.com>
2015-04-14 21:45:44 +0800

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
   def new
     @user = User.new
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
   
   def create
@@ -14,8 +22,27 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      # 处理更新成功的情况
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   def show
     @user = User.find(params[:id])
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:sccess] = "User deleted"
+    redirect_to users_url
   end
 
   private #对私有方法刻意多一层缩进
@@ -23,5 +50,23 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name,:email,:password,
                                    :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        store_lacation
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # 确保是管理员
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
